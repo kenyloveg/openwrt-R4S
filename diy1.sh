c3b3921b210a2cd358257fb0d5b6bdd2b0bb9585
openwrtledediy <57201365+openwrtledediy@users.noreply.github.com>
2022-09-24 23:37:15 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -16,10 +16,4 @@
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
